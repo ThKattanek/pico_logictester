32b77204b523db87aa63b4cc012f0d40ba98cd6b
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2026-02-21 13:05:54 +0100

    On Board LED wird beim Start des Pico eingeschaltet. Als Indikator das der Pico und die Firmware läuft.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -307,6 +307,12 @@ void DrawDisplayMask()
 int main()
 {
     stdio_init_all();
+
+    // onboard LED for debugging
+    const uint LED_PIN = 25;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 1); // Turn on LED to indicate startup
     
     // Initialize power management first
     InitPowerManagement();
